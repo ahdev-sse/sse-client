30a59b4236253f734b47bd987b8d2cffd6e16ac3
Alex Hui <ah_dev@outlook.com>
2022-07-05 04:12:53 -0700

    Create a simple client to display events

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import EchoingListener from './Components/EchoingListener'
+
+function App() {
+    return (
+        <div className="App">
+            Server Sent Events Demo
+            <EchoingListener />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/EchoingListener.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './EchoingListener.css'
+
+function EchoingListener() {
+    const [receivedEvents, setReceivedEvents] = useState([])
+
+    useEffect(() => {
+        const streamListener = new EventSource('http://localhost:3001/stream')
+        streamListener.onmessage = (event) => {
+            console.log(`Received: ${event.data}`)
+            setReceivedEvents((prev) => [...prev, event])
+        }
+        return () => {
+            console.log('Closing connection')
+            streamListener.close()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <div className="EchoingListener">
+            {receivedEvents.length ? (
+                <div>
+                    <div className="EchoingListener__LatestEvent">
+                        {receivedEvents[receivedEvents.length - 1].data}
+                    </div>
+                    <div className="EchoingListener__History">
+                    History:
+                    {receivedEvents.map((event) => (
+                        <div key={event.data}>
+                            {event.data}
+                        </div>
+                    ))}
+                    </div>
+                </div>
+            ) : undefined}
+        </div>
+    )
+}
+
+export default EchoingListener
